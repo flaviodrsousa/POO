d3cf1b55a58502b4b3898dbf4e810837021cd7d0
a97678 <a97678@alunos.uminho.pt>
2023-04-16 00:58:08 +0100

    Correcoes TA FUNCIONAL

--- Main.java
+++ Main.java
@@ -11,14 +11,22 @@ public class Main{
         2,"Confortaveis e bonitos","Adidas",47.5,0.8);
         System.out.println(art3);
 
-        /* Sapatilhas art4 = new Sapatilhas("sjgd4la",true,"Como novo",
-        2,"Confortaveis e bonitos","Adidas",47.5,0.8,42,true,"azul","2002-12-13",true);
-        System.out.println(art4); */
+        Sapatilha art4 = new Sapatilha("12345", true, "novo",
+        1, "Sapatilha de corrida", "Nike", 100.0,
+        0.9, 42, true, "preta", false,
+        "2022-01-01");
+        System.out.println(art4); 
 
-/*         Artigo art5 = new Artigo("sjgd4la",false,"Como novo",
-        2,"Confortaveis e bonitos","Adidas",47.5,
-        0.8,0,"Couro","2021-12-12",true);
-        System.out.println(art5); */
+        Mala art5 = new Mala("12345", true, "novo",
+        1, "Sapatilha de corrida", "Nike", 100.0,
+        0.9,2,"Couro","2022-12-23",true);
+        System.out.println(art5);
+
+        Tshirt art6 = new Tshirt("12345", true, "novo",
+        1, "Sapatilha de corrida", "Nike", 100.0,
+        0.9,2,2);
+        System.out.println(art6);
+
 
     }
     
